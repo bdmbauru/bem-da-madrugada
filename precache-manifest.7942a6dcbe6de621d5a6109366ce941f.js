d3a279290f4e165dabd4a17ffacbad4363bcdb4d
bdmbauru <bemdamadrugadabauru@gmail.com>
2020-08-24 19:37:21 -0300

    Updates

---
+++ precache-manifest.7942a6dcbe6de621d5a6109366ce941f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7a1952c1b0c433e1bb08dd5d4b26b5",
+    "url": "/bem-da-madrugada/index.html"
+  },
+  {
+    "revision": "d1775c43ce861d6d0be8",
+    "url": "/bem-da-madrugada/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "5b91a8582da011a96a44",
+    "url": "/bem-da-madrugada/static/js/2.08418748.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/bem-da-madrugada/static/js/2.08418748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1775c43ce861d6d0be8",
+    "url": "/bem-da-madrugada/static/js/main.2beab45b.chunk.js"
+  },
+  {
+    "revision": "d40288c700e89918f364",
+    "url": "/bem-da-madrugada/static/js/runtime-main.bcee9490.js"
+  },
+  {
+    "revision": "519b068400027f12a459e86f17012a9e",
+    "url": "/bem-da-madrugada/static/media/bdm2.519b0684.jpg"
+  },
+  {
+    "revision": "57b5f2c584877de9fc510b8b027c823f",
+    "url": "/bem-da-madrugada/static/media/bdm5.57b5f2c5.jpg"
+  },
+  {
+    "revision": "1ff8f26349b4547ac69f457c3f613e49",
+    "url": "/bem-da-madrugada/static/media/bdm7.1ff8f263.jpg"
+  },
+  {
+    "revision": "5476efd15562e63e8eb62e1a891c159b",
+    "url": "/bem-da-madrugada/static/media/bdm_desktop.5476efd1.jpg"
+  },
+  {
+    "revision": "56f8e94e1c9c025ffb5ca466d26987bf",
+    "url": "/bem-da-madrugada/static/media/bdm_mobile.56f8e94e.jpg"
+  },
+  {
+    "revision": "768e68af84b1684bf963a739cfa1f975",
+    "url": "/bem-da-madrugada/static/media/inicio.768e68af.png"
+  }
+]);
